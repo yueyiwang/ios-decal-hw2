06643888e3e81782d86e1350e76f35853e007899
Yueyi <annieyueyiwang@berkeley.edu>
2016-10-05 18:47:05 -0700

    functional calc

--- SwiftCalc/SwiftCalc/ViewController.swift
+++ SwiftCalc/SwiftCalc/ViewController.swift
@@ -22,7 +22,12 @@ class ViewController: UIViewController {
     // TODO: This looks like a good place to add some data structures.
     //       One data structure is initialized below for reference.
     var someDataStructure: [String] = [""]
-    
+    var stored: [String] = []
+    var input: String = ""
+    var curr_operator = ""
+    var int_result: Int = 0
+    var double_result: Double = 0
+    var prev_c = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,26 +51,82 @@ class ViewController: UIViewController {
     // TODO: A method to update your data structure(s) would be nice.
     //       Modify this one or create your own.
     func updateSomeDataStructure(_ content: String) {
-        print("Update me like one of those PCs")
+       // print("Update me like one of those PCs")
     }
     
     // TODO: Ensure that resultLabel gets updated.
     //       Modify this one or create your own.
     func updateResultLabel(_ content: String) {
-        print("Update me like one of those PCs")
+        if (content.characters.count > 7) {
+            resultLabel.text = content[content.startIndex...content.index(content.startIndex, offsetBy: 6)]
+        } else {
+            resultLabel.text = content
+        }
+        // print("Update me like one of those PCs")
     }
     
     
     // TODO: A calculate method with no parameters, scary!
     //       Modify this one or create your own.
     func calculate() -> String {
-        return "0"
+        if (!stored[0].contains(".") && !stored[1].contains(".")) {
+            let a:Int! = Int(stored[0])
+            let b:Int! = Int(stored[1])
+            var double = false
+            if (curr_operator == "/" && a % b != 0){
+                double_result = calculate(a: stored[0], b: stored[1], operation: curr_operator)
+                double = true
+            } else {
+                int_result = intCalculate(a: a!, b: b!, operation: curr_operator)
+            }
+            var r:String! = ""
+            if (double) {
+                r = String(double_result)
+            } else {
+                r = String(int_result)
+            }
+            updateResultLabel(r!)
+            curr_operator = ""
+            stored = [r]
+            input = ""
+            return r
+        } else {
+            double_result = calculate(a: stored[0], b: stored[1], operation: curr_operator)
+            var r:String! = ""
+            if (double_result == floor(double_result)) {
+                print("converting to int")
+                let i: Int! = Int(double_result)
+                r = String(i)
+            } else {
+                print("still a double")
+                let floored = floor(double_result)
+                let i = Int(double_result)
+                print("double: \(double_result)")
+                print("floored: \(floored)")
+                print("int: \(i)")
+                r = String(double_result)
+            }
+            updateResultLabel(r!)
+            curr_operator = ""
+            stored = [r]
+            input = ""
+            return r
+        }
     }
     
     // TODO: A simple calculate method for integers.
     //       Modify this one or create your own.
     func intCalculate(a: Int, b:Int, operation: String) -> Int {
         print("Calculation requested for \(a) \(operation) \(b)")
+        if (operation == "+") {
+            return a + b
+        } else if (operation == "-") {
+            return a - b
+        } else if (operation == "*") {
+            return a * b
+        } else if (operation == "/") {
+            return a / b
+        }
         return 0
     }
     
@@ -73,6 +134,17 @@ class ViewController: UIViewController {
     //       Modify this one or create your own.
     func calculate(a: String, b:String, operation: String) -> Double {
         print("Calculation requested for \(a) \(operation) \(b)")
+        let a:Double! = Double(a)
+        let b:Double! = Double(b)
+        if (operation == "+") {
+            return a + b
+        } else if (operation == "-") {
+            return a - b
+        } else if (operation == "*") {
+            return a * b
+        } else if (operation == "/") {
+            return a / b
+        }
         return 0.0
     }
     
@@ -80,16 +152,124 @@ class ViewController: UIViewController {
     func numberPressed(_ sender: CustomButton) {
         guard Int(sender.content) != nil else { return }
         print("The number \(sender.content) was pressed")
+        if (curr_operator == "") {
+            stored = []
+        }
+        if (curr_operator != "" && stored.count == 2) {
+           /* if (!stored[0].contains(".") && !stored[1].contains(".")) {
+                let a:Int? = Int(stored[0])
+                let b:Int? = Int(stored[1])
+                int_result = intCalculate(a: a!, b: b!, operation: curr_operator)
+                let r:String? = String(int_result)
+                updateResultLabel(r!)
+                curr_operator = ""
+                stored = []
+            } else {
+                double_result = calculate(a: stored[0], b: stored[1], operation: curr_operator)
+                let r:String? = String(double_result)
+                updateResultLabel(r!)
+                curr_operator = ""
+                stored = []
+                
+            }*/
+        } else {
+            input += sender.content
+            updateResultLabel(input)
+        }
+        prev_c = false
         // Fill me in!
     }
     
     // REQUIRED: The responder to an operator button being pressed.
     func operatorPressed(_ sender: CustomButton) {
+        print("Operater \(sender.content) was pressed")
+        if (sender.content == "=") {
+            // print ("stored: \(input)")
+            stored += [input]
+            if (stored.count == 0) {
+            } else if (curr_operator != "" && stored.count == 2) {
+                print("doing operation")
+                calculate()
+            }
+            prev_c = false
+            
+        } else if (sender.content == "C") {
+            if (prev_c || curr_operator == "") {
+                prev_c = true
+                updateResultLabel("0")
+                input = ""
+                stored = []
+                curr_operator = ""
+            } else {
+                prev_c = true
+                updateResultLabel(stored[0])
+                input = ""
+                if (curr_operator == "") {
+                    stored = []
+                }
+            }
+        } else if (sender.content == "+/-") {
+            let label: String! = resultLabel.text
+            if ((input != "" && input[input.startIndex] == "-") ||
+                (label != "" && label[label.startIndex] == "-")) {
+                print("becoming positive")
+                var update = ""
+                var count = 0
+                for s in input.characters {
+                    if (count == 0) {
+                        count = count + 1
+                    } else {
+                        update += String(s)
+                    }
+                }
+                input = update
+            } else if (input != "" || label != "") {
+                print("becoming negative")
+                var update = "-"
+                for s in input.characters {
+                    update += String(s)
+                }
+                input = update
+            }
+            prev_c = false
+            updateResultLabel(input)
+        } else {
+            // print("Num Operator \(sender.content) was pressed")
+            if (stored.count == 1 && input == "") {
+                curr_operator = sender.content
+            } else {
+                stored += [input]
+            }
+            if (curr_operator != "" && stored.count == 2) {
+                print("doing operation")
+                calculate()
+                curr_operator = sender.content
+            } else {
+                curr_operator = sender.content
+                input = ""
+                // updateResultLabel("0")
+            }
+            prev_c = false
+        }
         // Fill me in!
     }
     
     // REQUIRED: The responder to a number or operator button being pressed.
     func buttonPressed(_ sender: CustomButton) {
+        print("button \(sender.content) pressed")
+        if (sender.content == ".") {
+            if (!input.contains(".")) {
+                input += "."
+                updateResultLabel(input)
+            }
+        } else if (sender.content == "0") {
+            if (input != "0") {
+                input += "0"
+                updateResultLabel(input)
+            }
+        }
+        prev_c = false
+        print("check")
        // Fill me in!
     }
     
